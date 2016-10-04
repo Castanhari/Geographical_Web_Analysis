d6ff16a9d4031e720c444b8ba825ed1cf7c95ee4
Castanhari <raul.icmcjr@gmail.com>
2016-10-04 10:19:44 -0300

    Update ui.R

--- timeSeriesApp/ui.R
+++ timeSeriesApp/ui.R
@@ -2,9 +2,12 @@ library(shiny)
 
 shinyUI
 (  ui <- fluidPage(
-     titlePanel("Time Series"),
+     selectInput
+      (  inputId="option",
+         label="Time Series options",
+         choices=c("time_series", "bfast01(time_series)", "bfast(time_series)", "bfastmonitor(time_series)", "twdtw(time_series)")
+      ),
       
       plotOutput("plot")
    )
 )
-
