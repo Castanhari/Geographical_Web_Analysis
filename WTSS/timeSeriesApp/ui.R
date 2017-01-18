bfa9420f72a0ad3da3a27c070962e0acb490884d
Castanhari <raul.icmcjr@gmail.com>
2017-01-18 11:03:13 -0200

    Update ui.R

--- WTSS/timeSeriesApp/ui.R
+++ WTSS/timeSeriesApp/ui.R
@@ -1,16 +1,37 @@
 library(shiny)
 
 shinyUI
-(  ui <- fluidPage (
-     singleton(tags$head(tags$script(src="handlers.js"))),
+(  ui <- fluidPage(
+      singleton(tags$head(tags$script(src="handlers.js"))),
       
-      selectInput
-      (  inputId="option",
-         label="Time Series options",
-         choices=c('', "time_series", "bfast01(time_series)", "bfast(time_series)", "bfastmonitor(time_series)", "twdtw(time_series)")
+      fillRow
+      (  flex=c(NA, 1, NA, 1, 10),
+         
+         selectInput
+         (  inputId="option",
+            label="Filter",
+            choices=c('', "time_series", "bfast01(time_series)", "bfast(time_series)", "bfastmonitor(time_series)", "twdtw(time_series)"),
+            width="210px"
+         ),
+         
+         div(),
+         
+         selectInput
+         (  inputId="attributes",
+            label="Attributes",
+            choices=character(0),
+            multiple=TRUE,
+            width="155px"
+         ),
+         
+         div(),         
+         
+         uiOutput("dates_interface")
       ),
       
+      br(),
+      
       uiOutput("result_interface")
+      
    )
 )
-
